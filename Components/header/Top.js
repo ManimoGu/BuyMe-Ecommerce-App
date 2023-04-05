1cf7d293d34fa11f751597c764eed55fead1d1f0
ManimoGu <guennach.imane@gmail.com>
2023-04-05 15:08:19 +0000

    Ads top

---
+++ Components/header/Top.js
@@ -0,0 +1,43 @@
+import styles from "./styles.module.scss";
+import { MdSecurity } from "react-icons/md";
+import {BsSuitHeart} from "react-icons/bs"
+import {RiAccountPinCircleLine,RiArrowDropDownFill } from "react-icons/ri"
+import Link from "next/link";
+
+export default function Top() {
+  return (
+    <div className={styles.Top}>
+      <div className={styles.top_container}>
+        <div></div>
+        <ul className={styles.top_list}>
+          <li>
+            <img src="https://www.seekpng.com/png/full/323-3232715_morocco-flag-png-angel-tube-station.png" alt="" />
+            <span>Morocco / usd</span>
+          </li>
+          <li>
+            <MdSecurity />
+            <span>Buyer Protection</span>
+          </li>
+          <li>
+            <span>Customer Service</span>
+          </li>
+          <li>
+            <span>Help</span>
+          </li>
+          <li>
+            <BsSuitHeart/>
+            <Link href="/profile/whishlist"><span>Whishlist</span></Link>
+            
+          </li>
+          <li>
+            <div className={styles.flex}>
+                <RiAccountPinCircleLine/>
+                <span>Account</span>
+                <RiArrowDropDownFill/>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
