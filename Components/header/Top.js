bec1edc87a06a6729b5029eaf7940fc3e8738953
ManimoGu <guennach.imane@gmail.com>
2023-04-08 11:23:21 +0000

    menu part1

--- Components/header/Top.js
+++ Components/header/Top.js
@@ -7,40 +7,41 @@ import { useState } from "react";
 import UserMenu from "./UserMenu";
 
 export default function Top() {
-  const [logged, setLogged] = useState(false);
+  const [logged, setLogged] = useState(true);
+  const [visible, setvisible] = useState(false);
 
   return (
     <div className={styles.Top}>
       <div className={styles.top_container}>
         <div></div>
         <ul className={styles.top_list}>
-          <li>
+          <li className={styles.li}>
             <img
               src="https://www.seekpng.com/png/full/323-3232715_morocco-flag-png-angel-tube-station.png"
               alt=""
              
             />
             <span>Morocco / usd</span>
-          </li>
-          <li>
+          </li >
+          <li className={styles.li}>
             <MdSecurity />
             <span>Buyer Protection</span>
           </li>
-          <li>
+          <li className={styles.li}>
             <span>Customer Service</span>
           </li>
-          <li>
+          <li className={styles.li}>
             <span>Help</span>
           </li>
-          <li>
+          <li className={styles.li}>
             <BsSuitHeart />
             <Link href="/profile/whishlist">
               <span>Whishlist</span>
             </Link>
           </li>
-          <li>
+          <li className={styles.li} onMouseOver={()=>setvisible(true)} onMouseLeave={()=>setvisible(false)}>
           {logged ? (
-            <li>
+            <li className={styles.li}>
               <div className={styles.flex}>
                 <img src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png" alt="" />
                 <span>Imane</span>
@@ -48,7 +49,7 @@ export default function Top() {
               </div>
             </li>
           ) : (
-            <li>
+            <li className={styles.li} >
               <div className={styles.flex}>
                 <RiAccountPinCircleLine />
                 <span>Account</span>
@@ -56,7 +57,10 @@ export default function Top() {
               </div>
             </li>
           )}
-         <UserMenu logged = {logged}/>
+          {
+            visible && <UserMenu logged = {logged}/>
+          }
+      
           </li>
           
         </ul>
