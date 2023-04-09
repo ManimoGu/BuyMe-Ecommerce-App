db57e514ba3a055baa6f3ebd1426918dd3f6ba51
ManimoGu <guennach.imane@gmail.com>
2023-04-09 16:39:28 +0000

    footer first part

---
+++ Components/Footer/Links.js
@@ -0,0 +1,109 @@
+import { imageConfigDefault } from "next/dist/shared/lib/image-config"
+import styles from "./styles.module.scss"
+import Link from "next/link"
+
+const Links = () => {
+  return (
+    <div className={styles.footer_links}>
+        {
+            links.map((link,i) =>(
+                <ul>
+                    {
+                        i===0 ? (<img src="../../../logo.png" alt="" /> )
+                        :
+                        (
+                            <b>{link.heading}</b>
+                        )
+
+                    }
+                    
+                    {
+                        link.links.map(link =>(
+                           <li>
+                            <Link href={link.link}><span>{link.name}</span></Link>
+                           </li>
+                        ))
+                    }
+                </ul>
+            ))
+        }
+    </div>
+  )
+}
+
+const links =[
+    {
+        heading : "SHOPPAY",
+        links : [
+            {
+                name : "About us",
+                link : ""
+            },
+            {
+                name : "Contact us",
+                link : ""
+            },
+            {
+                name : "Social Responsibility",
+                link : ""
+            },
+            {
+                name : "",
+                link : ""
+            }
+        ]
+    },
+    {
+        heading : "Help & Support",
+        links :
+         [
+            {
+                name : "Shipping Info",
+                link : ""
+            },
+            {
+                name : "Returns",
+                link : ""
+            },
+            {
+                name : "How to order",
+                link : ""
+            },
+            {
+                name : "How to track",
+                link : ""
+            },
+            {
+                name : "Size guide",
+                link : ""
+            }
+
+        ]
+    },
+    {
+        heading : "Customer service",
+        links :
+         [
+            {
+                name : "Customer setvice",
+                link : ""
+            },
+            {
+                name : "Terms and Conditions",
+                link : ""
+            },
+            {
+                name : "Customers Transactions",
+                link : ""
+            },
+            {
+                name : "Take our feedback survey",
+                link : ""
+            }
+        ]
+    }
+]
+
+
+
+export default Links
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.scss'
 import Header from '@/Components/header'
+import Footer from '@/Components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,7 @@ export default function Home() {
       <main className={styles.main}>
 
        <Header/>
+       <Footer/>
        
       </main>
     </>
